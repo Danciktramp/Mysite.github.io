a5ec28339dd37443f704aed931de3eba2a858399
Danciktramp <bolozandan@gmail.com>
2025-11-08 11:42:26 +0200

    Add files via upload

---
+++ js.js
@@ -0,0 +1,11 @@
+
+function w3_open() {
+  document.getElementById("mySidebar").style.display = "block";
+}
+
+function w3_close() {
+  document.getElementById("mySidebar").style.display = "none";
+}
+function openlink1(){
+    window.open('https://www.w3schools.com/js/js_scope.asp', '_blank');
+}
